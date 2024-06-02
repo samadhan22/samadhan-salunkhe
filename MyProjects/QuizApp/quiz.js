0c958a1e5f6b3854ca000d1f00776387ce850669
samadhan22 <samadhansalunkhe22@gmail.com>
2024-06-02 10:54:56 +0530

    Quiz App Setup

---
+++ MyProjects/QuizApp/quiz.js
@@ -0,0 +1,9 @@
+const questionObj = 
+    {
+      category: 'Food & Drink',
+      id: 'qa-1',
+      correctAnswer: 'Three',
+      answers: ['Two', 'Three ', 'Four', 'Five'],
+      question:
+        "How many pieces of bun are in a Mcdonald's Big Mac?",
+    };
